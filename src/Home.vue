f5c764bd0a873f76cbdae54e7de1b00bedc50030
Boris Civcic <civcicboris93@gmail.com>
2019-07-25 14:43:55 +0200

    add: Homepage structure

--- src/Home.vue
+++ src/Home.vue
@@ -1,27 +1,22 @@
 <template>
-  <v-app>
-    <app-navigation></app-navigation>
+  <span>
     <home-hero></home-hero>
     <home-details></home-details>
     <home-plans></home-plans>
-  </v-app>
+  </span>
 </template>
 
 <script>
-import AppNavigation from "@/components/AppNavigation";
 import HomeHero from "@/components/HomeHero";
 import HomeDetails from "@/components/HomeDetails";
 import HomePlans from "@/components/HomePlans";
+
 export default {
-  name: "App",
+  name: "home",
   components: {
-    AppNavigation,
     HomeHero,
     HomeDetails,
     HomePlans
   }
 };
 </script>
-
-<style>
-</style>
